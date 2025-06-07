02386084d107e7183af12b7087b565cd6ee8858e
wjdtmdgP1 <qwrt22224@gmail.com>
2025-06-08 02:24:27 +0900

    표지판 - Sign 프리팹 생성 - Canvas에 있는 signUI 오브젝트를 이용해서 UI를 표시합니다 - 안내 UI는 한 번에 하나만 보이게 설정해야 합니다 - Sign 프리팹을 원하는 곳에 배치하고 인스펙터에서 문구를 설정해주세요

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour
+{
+    [SerializeField] private string sign_ment = "null";
+    [SerializeField] private Transform uiPosition;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Mimic")
+        {
+            GameManager.Instance.ShowSignUI(sign_ment, uiPosition, true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        GameManager.Instance.UpdateSignUI(uiPosition);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Mimic")
+        {
+            GameManager.Instance.ShowSignUI(sign_ment, uiPosition, false);
+        }
+    }
+}
